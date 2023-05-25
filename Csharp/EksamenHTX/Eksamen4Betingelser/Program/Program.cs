a3bb454d1be481f7b403d502550d4cbc284cee19
Gjellerhjort <mads.gjellerod@gmail.com>
2023-05-25 10:57:47 +0200

    Yes one more lets gooo

---
+++ Csharp/EksamenHTX/Eksamen4Betingelser/Program/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ProgramShit
+{
+    class Program
+    {
+        int tal1;
+        int tal2;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast to tal og find det største");
+            Console.WriteLine("1.Tal:");
+            var tal1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("2.Tal:");
+            var tal2 = Int32.Parse(Console.ReadLine());
+
+            if (tal1 > tal2)
+            {
+                Console.WriteLine("Tal1 er størst: " + tal1);
+            } else
+            {
+                Console.WriteLine("Tal2 er størst: " + tal2);
+            }
+        }
+    }
+}
