d362a7fd0008dbcdf6169ca71fdd3ef796a3e07d
gjellerhjort <mads.gjellerod@gmail.com>
2023-05-26 19:17:23 +0200

    somwthing

--- Microchips/Arduino/CarValveSystem/CarValveSystem.ino
+++ Microchips/Arduino/CarValveSystem/CarValveSystem.ino
@@ -1,22 +1,33 @@
 volatile byte rpmcount = 0;
 unsigned long rpm = 0;
 int interruptPin = 3;
-int lift = 60; // dette er i ms
-int duration = 100; // dette er i ms
+int lift = 30; // dette er i ms
+int duration = 15; // dette er i ms
+int BUTTON_PIN = 2;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin),rpmtrigger,FALLING);
+  pinMode(2, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  attachInterrupt(2, RpmMode, FALLING); 
 }
 
 
 
 void loop() {
+
+  lift = analogRead(A0);
+  duration = analogRead(A1);
+  duration = map(duration, 0, 1023,30, 400);
+  lift = map(lift, 0, 1023,30, 400);
+  Serial.println(lift);
+  Serial.println(rpm);
   digitalWrite(5, HIGH);
   digitalWrite(6, HIGH);
-  delay(lift+ );
+  delay(duration);
   digitalWrite(5, LOW);
   digitalWrite(6, LOW);
   delay(lift);
@@ -25,4 +36,13 @@ void loop() {
 void rpmtrigger()
 {
  rpmcount++;
+}
+
+void RpmMode() {
+  rpm = rpm + 1000;
+  if (rpm > 10000)
+  
+{
+  rpm=0;      
+}  
 }
