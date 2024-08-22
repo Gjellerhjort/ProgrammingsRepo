891f0bc241e36382044142d9f279ffca88ec9369
gjellerhjort <mads.gjellerod@gmail.com>
2024-08-22 11:26:38 +0200

    working

--- Python/Projekter/sus-mad/tests.py
+++ Python/Projekter/sus-mad/tests.py
@@ -1,8 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
 import urllib.parse
-#class SallingFood:
 
+class FoodItem:
+    def __init__(self, name, price, price_per_kilogram, price_per_unit, unit):
+        self.name = name
+        self.price = float(price)  # Ensure price is stored as a float
+        self.price_per_kilogram = float(price_per_kilogram)
+        self.price_per_unit = float(price_per_unit)
+        self.unit = str(unit)
+
+
+    def __repr__(self):
+        return f"FoodItem(name='{self.name}', price={self.price}, price per kg={self.price_per_kilogram}, price per unit={self.price_per_unit})"
+
+    def __str__(self):
+        return f"{self.name}: {self.price_per_unit} DKK per kg"
+
+    def get_name(self):
+        return self.name
+    
+    def get_price(self):
+        return self.price
+    
+    def get_price_per_kilogram(self):
+        return self.price_per_kilogram
+
+    def get_price_per_unit(self):
+        return self.price_per_unit
+
+class SallingFood:
+    salling_api = ""
+    url = "https://api.sallinggroup.com/v1-beta/product-suggestions/relevant-products?query="
+    headers = {
+    'Authorization': "Bearer " + salling_api
+    }
+    def GetFood(self, food):
+        encoded_string = urllib.parse.quote(food)
+        url = self.url + encoded_string + "/"
+        response = requests.get(url, headers=self.headers)
+        print(response.json())
 
 
 class RemaFood:
@@ -20,21 +57,20 @@ class RemaFood:
         data = response.json()
         return data.get('hits', [])
     
-    def GetFoodList(self, food):
-        food_list = []
+    def GetFoodItems(self, food):
+        foodItem_list = []
         for items in self.GetFood(food):
-            item = {
-                "name" : str(items["name"]), "price" : str(items["pricing"]["price_per_kilogram"])
-            }
-            food_list.append(item)
+            price_per_unit = str(items['pricing']['price_per_unit'])
+            price, unit_str = price_per_unit.split(" ", 1)
+            item = FoodItem(items['name'], float(items['pricing']['price']), float(items['pricing']['price_per_kilogram']), float(price), unit_str)
+            foodItem_list.append(item)
 
-        return food_list
+        return foodItem_list
     #def GetPrices(self, food):
 
     def GetBestPrice(self, food):
-        food_list = self.GetFoodList(food)
-        print(food_list)
-        cheapest_food = min(food_list, key=lambda x:x['price'])
+        food_list = self.GetFoodItems(food)
+        cheapest_food = min(food_list, key=lambda x:x.get_price_per_unit())
 
         print(cheapest_food)
 
@@ -43,9 +79,12 @@ class RemaFood:
 
 
 rema = RemaFood()
+salling = SallingFood()
+
+
 while True:
     food_item = input("find pris: ")
-    rema.GetBestPrice(food_item)
+    salling.GetFood(food_item)
 
 
 
