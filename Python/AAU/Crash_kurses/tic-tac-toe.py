b119e28dad2fb855e1b260d3b3432163c5cf4418
gjellerhjort <mads.gjellerod@gmail.com>
2024-09-11 12:12:20 +0200

    small changes

--- Python/AAU/Crash_kurses/tic-tac-toe.py
+++ Python/AAU/Crash_kurses/tic-tac-toe.py
@@ -1,24 +1,29 @@
 
+# printer game board 
 def PrintBoard(board):
     for i in range(0, 8, 3):
         print(board[i]+board[i+1]+board[i+2])
 
+# tjekker om person har fundet
 def Win(player):
     print(f"{player["Name"]} won this game of tic tac toe")
     main()
 
+# tjekker om spiller har vundet 
 def CheckBoard(player, board):
-    # check x win
     for i in range(0,3):
+        # tjekker horisonal linjer
         if board[i] == board[i+1] == board[i+2] != "-":
             Win(player)
+        # tjekker verticale linjer
         if board[i] == board[i+3] == board[i+6] != "-":
             Win(player)
+    # tjekker skraa linjer
     if board[0] == board[4] == board[8] != "-":
         Win(player)
     if board[2] == board[4] == board[6] != "-":
         Win(player)
-
+# soette broek på boardet
 def Boardmove(player, board):
     position = input(f"{player["Name"]} set a {player["playerIcon"]}: ")
     if position == None or not position.isdigit():
@@ -31,13 +36,14 @@ def Boardmove(player, board):
         Boardmove(player, board)
     CheckBoard(player, board)
 
+# laver board til spillet og lave to spillere som kan sætte deres navn
 def GameSetup():
     board = ["-", "-", "-", 
              "-", "-", "-", 
              "-", "-", "-"]
 
-    player1 = {"Name" : "", "playerIcon": "x", "plays": []}
-    player2 = {"Name": "", "playerIcon": "o", "plays": []}  
+    player1 = {"Name" : "", "playerIcon": "x"}
+    player2 = {"Name": "", "playerIcon": "o"}  
 
     name = input("Player 1 choose a name: ")
     player1["Name"] = name
@@ -45,6 +51,7 @@ def GameSetup():
     player2["Name"] = name
     return board, player1, player2
 
+# kalder de forskellige funktioner
 def main():
     board, player1, player2 = GameSetup()
     PrintBoard(board)
