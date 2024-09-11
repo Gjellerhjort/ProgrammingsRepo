b254a1386701518b493eed78fffae5be93b1eda7
gjellerhjort <mads.gjellerod@gmail.com>
2024-09-11 09:28:12 +0200

    game working

--- Python/AAU/Crash_kurses/tic-tac-toe.py
+++ Python/AAU/Crash_kurses/tic-tac-toe.py
@@ -1,46 +1,61 @@
-board = ["-", "-", "-", 
-         "-", "-", "-", 
-         "-", "-", "-"]
 
-player1 = {"Name" : "", "playerIcon": "x", "plays": []}
-player2 = {"Name": "", "playerIcon": "o", "plays": []}  
-
-def PrintBoard():
+def PrintBoard(board):
     for i in range(0, 8, 3):
         print(board[i]+board[i+1]+board[i+2])
 
 def Win(player):
     print(f"{player["Name"]} won this game of tic tac toe")
+    main()
 
-def CheckBoard(player):
+def CheckBoard(player, board):
     # check x win
     for i in range(0,3):
         if board[i] == board[i+1] == board[i+2] != "-":
             Win(player)
-        if board[i] == board[i+3] == board[i+5] != "-":
+        if board[i] == board[i+3] == board[i+6] != "-":
             print(board[i] + board[i+3] + board[i+5])
             Win(player)
-        
+    if board[0] == board[4] == board[8] != "-":
+        Win(player)
+    if board[2] == board[4] == board[6] != "-":
+        Win(player)
 
-def Boardmove(position, player):
-    board[int(position)-1] = player["playerIcon"]
-    CheckBoard(player)
+def Boardmove(player, board):
+    position = input(f"{player["Name"]} set a {player["playerIcon"]}: ")
+    if position == None or not position.isdigit():
+        print("Please type a number")
+        Boardmove(player, board)
+    if board[int(position)-1] == "-":
+        board[int(position)-1] = player["playerIcon"]
+    else:
+        print("You can place here")
+        Boardmove(player, board)
+    CheckBoard(player, board)
 
 def GameSetup():
+    board = ["-", "-", "-", 
+             "-", "-", "-", 
+             "-", "-", "-"]
+
+
+    player1 = {"Name" : "", "playerIcon": "x", "plays": []}
+    player2 = {"Name": "", "playerIcon": "o", "plays": []}  
+
     name = input("Player 1 choose a name: ")
     player1["Name"] = name
     name = input("Player 2 choose a name: ")
     player2["Name"] = name
+    return board, player1, player2
 
 def main():
-    GameSetup()
+
+    board, player1, player2 = GameSetup()
+    PrintBoard(board)
     while True:
-        PrintBoard()
-        position = input(f"{player1["Name"]} set a x: ")
-        Boardmove(position, player1)
-        PrintBoard()
-        position = input(f"{player2["Name"]} set a o: ")
-        Boardmove(position, player2)
+        Boardmove(player1, board)
+        PrintBoard(board)
+        Boardmove(player2, board)
+        PrintBoard(board)
 
 
 
