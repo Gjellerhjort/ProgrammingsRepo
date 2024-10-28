6791500c933f7746ca3921e8424ca6deffe18905
gjellerhjort <mads.gjellerod@gmail.com>
2024-10-28 21:23:21 +0100

    more tasks

--- Python/AAU/Grundlæggende_programmering/4.Filer_moduler_og_databehandling/opgave6.py
+++ Python/AAU/Grundlæggende_programmering/4.Filer_moduler_og_databehandling/opgave6.py
@@ -1,14 +1,13 @@
 import pandas as pd
 
 
-# opgave 6.1 og 6.4
+# opgave 6.1
 
 # Opret en ordbog med dataene
 data = {
     "Fødselsdato": ["24/12/2001", "02/02/1998", "12/11/1999", "31/01/2003", "07/08/2000"],
     "By": ["Randers", "Åbybro", "Aalborg", "Nibe", "Aalborg"],
     "Køn": ["Mand", "Mand", "Kvinde", "Kvinde", "Mand"],
-    "Børn": [1,0,2,0,0]
 }
 
 # Navnene bruges som indeks
@@ -22,3 +21,14 @@ print(df.columns)
 
 # opgave 6.3
 print(df.loc["Dorthe Damsgaard"])
+
+# opgave 6.4
+df["Børn"] = [1,0,2,0,0]
+print(df)
+
+# opgave 6.5
+
+kvinder = df[df["Køn"] == "Kvinde"]
+
+print(kvinder)
+# opgave 6.6
